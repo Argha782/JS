e89088e2542cbf943637bb1885b5b30014b5321f
Argha Saha <113825499+Argha782@users.noreply.github.com>
2025-02-14 16:35:30 +0530

    06 file modified

--- 06stack_heap.js
+++ 06stack_heap.js
@@ -6,3 +6,14 @@ anothername = "Phantom"
 console.log(youtubename);
 console.log(anothername);
 
+let user1 = {
+    email: "arghasaha234@mail.com",
+    upi: "okaxis222"
+}
+
+let user2 = user1
+user2.email = "argha23@gmail.com"
+
+console.log(user1);
+console.log(user2);
+
