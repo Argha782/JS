a2ce27bf69df35daccf71c8fd1fd12ddba15a504
Argha Saha <113825499+Argha782@users.noreply.github.com>
2025-02-15 15:02:26 +0530

    07 & 08 files added

---
+++ 07strings.js
@@ -0,0 +1,44 @@
+// const name = "Argha"
+// const repoCount = 500
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// const username = new String('Hitesh')
+
+// console.log(name.length);
+// console.log(name[0]);
+// console.log(username.length);
+// console.log(username[2]);
+// console.log(username.toUpperCase());
+// console.log(username.toLowerCase());
+// console.log(username.charAt(4));
+// console.log(username.indexOf('t'));
+
+// const newString = username.substring(0,3)
+// console.log(newString);
+
+// const anotherString = username.slice(-9,3)
+// console.log(anotherString);
+
+// console.log(username.slice(2,5));
+// console.log(username.substring(2,5));
+// console.log(username.substr(2,5));
+// console.log(username.slice(3));
+
+ text1 = "   Argha Saha  "
+ console.log(text1.trim());
+ console.log(text1.trimStart());
+ console.log(text1.trimEnd());
+
+ text2 = "5"
+ console.log(text2.padStart(6,"x"));
+ console.log(text2.padEnd(6,"x"));
+ console.log(text1.repeat(4));
+ console.log(text1.replace("Saha","Das"));
+
+
+ 
+
+ 
+ 
+
---
+++ 08numsandmaths.js
@@ -0,0 +1,19 @@
+// const score = 200
+// const balance = new Number(100)
+// console.log(score);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const num1= 23.8966
+// console.log(num1.toPrecision(3));
+
+// const hundred = 10000000
+// console.log(hundred.toLocaleString());
+// console.log(hundred.toLocaleString('en-UK'));
+
+// ******************************MATHS**********************************************
+
+
+console.log(Math);
+
