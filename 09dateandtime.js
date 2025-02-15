877ca2cd63374adcc5e2e062d7fd3783d5561523
Argha Saha <113825499+Argha782@users.noreply.github.com>
2025-02-15 16:35:46 +0530

    File 09 added

---
+++ 09dateandtime.js
@@ -0,0 +1,26 @@
+// let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toTimeString());
+// console.log(typeof myDate);
+// let createdDate = new Date(2025, 0, 23, 5, 3)
+// console.log(createdDate.toLocaleString());
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(createdDate);
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getTime());
+console.log(newDate.getDay());
+console.log(newDate.getMinutes());
+console.log(newDate.getFullYear());
+console.log(newDate.getMonth()+1);
+
