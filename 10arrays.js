752e144268dad9b6fc00dda126a74306802dcdb5
Argha Saha <113825499+Argha782@users.noreply.github.com>
2025-02-18 15:47:32 +0530

    files 10 and 11 on arrays added

---
+++ 10arrays.js
@@ -0,0 +1,27 @@
+const cars =["BMW", "Audi","Mercedes", "Lamborghini"]
+console.log(cars);
+// console.log(cars.length);
+// console.log(cars[2]);
+// console.log(cars.join("*"));
+// console.log(cars.pop());
+// console.log(cars);
+// console.log(cars.push("Rolls Royce"));
+// console.log(cars);
+// console.log(cars.shift());
+// console.log(cars);
+// console.log(cars.unshift("Hyundai"));
+// console.log(cars);
+cars[0]="Hyundai";
+console.log(cars);
+let myarray =[0, 1, 2, 3, 4, 5]
+// console.log(myarray.splice(2, 0, 7));
+// console.log(myarray);
+// console.log(myarray.splice(2, 2, 9));
+// console.log(myarray);
+// console.log(myarray.indexOf(2));
+// myarray1 =[true, false, 0, "Argha"]
+// console.log(cars.concat(myarray));
+// console.log("A",cars.concat(myarray,myarray1));
+console.log(myarray);
+console.log(myarray.slice(1,3));
+
---
+++ 11arrays2.js
@@ -0,0 +1,21 @@
+marvel = ["thor", "hulk", "iron  man"]
+dc = ["batman", "flash", "superman",]
+marvel.push(dc)
+console.log(marvel);
+console.log(marvel[3][1]);
+newArray = [1, 2, 3, [6, 7, 10], 5, [4, 11, [20, 15]], 17]
+console.log(newArray.flat());
+console.log(newArray.flat(2));
+console.log(newArray.flat(Infinity));
+console.log(Array.isArray("Argha"));
+console.log(Array.from("Argha"));
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+let score4 = 400
+console.log(Array.of(score1,score2,score3,score4));
+
+
+
+
