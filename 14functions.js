298b21814718f2e18bf14347571662c807fc410e
Argha Saha <113825499+Argha782@users.noreply.github.com>
2025-02-19 16:54:33 +0530

    Modified file 14

---
+++ 14functions.js
@@ -0,0 +1,60 @@
+function sayMyname(){
+    console.log("A")
+    console.log("R")
+    console.log("G")
+    console.log("H")
+    console.log("A")
+}
+
+// sayMyname()
+
+function addnum(num1,num2){
+    console.log(num1 + num2)
+}
+// function addnum(num1,num2){
+//     result=num1 + num2
+//     console.log("Hitesh");
+//     return result
+//     console.log("Hitesh"); //won't be printed after result
+    
+// }
+// addnum(3,4)
+// addnum(3,"4")
+// addnum(3,"a")
+// addnum(3,null)
+
+// function loginUsermessage(username){
+//     if(username===undefined){           // if(!username){
+//         console.log("Plese enter username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+// console.log(loginUsermessage("Argha"));
+// console.log(loginUsermessage(""));
+// console.log(loginUsermessage());
+
+function calcprice(...num1)
+{
+    return num1
+}
+
+console.log(calcprice(200,300,400));
+
+const item={
+    product: "laptop",
+    price: 30000
+}
+
+function handleobj(anyitem){            // functions with obj
+    console.log(`Product name is ${anyitem.product} and its price is ${anyitem.price}`);
+}
+handleobj(item)
+
+const newArr=[100,200,200]
+function handlearray(anyArr){           // functions with array
+    // console.log(anyArr[1]);
+    return anyArr[1]
+}
+// handlearray(newArr)
+console.log(handlearray(newArr));
