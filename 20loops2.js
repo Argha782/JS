c600f5e9d469c317a42188d6e0ae6b6abfe3a184
Argha Saha <113825499+Argha782@users.noreply.github.com>
2025-02-27 17:01:15 +0530

    20loops modified

--- 20loops2.js
+++ 20loops2.js
@@ -1,33 +1,33 @@
-heros=["batman","hulk","spiderman"]
-for (const i of heros) {
-    console.log(`Name of hero ${i}`);
-}
- const map= new Map()
- map.set('IND',"India")
- map.set('US',"Unites States")
- map.set('FRA',"France")
- console.log(map);
+// heros=["batman","hulk","spiderman"]
+// for (const i of heros) {
+//     console.log(`Name of hero ${i}`);
+// }
+//  const map= new Map()
+//  map.set('IND',"India")
+//  map.set('US',"Unites States")
+//  map.set('FRA',"France")
+//  console.log(map);
  
- for (const [key,value] of map) {
-    console.log(key, ":-", value);
- }
+//  for (const [key,value] of map) {
+//     console.log(key, ":-", value);
+//  }
 
- const languages={
-    "js": "JavaScript",
-    "cpp": "C++",
-    "rb": "Ruby"
- }
-for (const key in languages) {
-   console.log(`${key} is the short form of ${languages[key]}`);
-   }
+//  const languages={
+//     "js": "JavaScript",
+//     "cpp": "C++",
+//     "rb": "Ruby"
+//  }
+// for (const key in languages) {
+//    console.log(`${key} is the short form of ${languages[key]}`);
+//    }
 
-const coding =["js", "cpp", "python", "java", "fortran"]
+// const coding =["js", "cpp", "python", "java", "fortran"]
 // coding.forEach(function(val){
 //     console.log(val);
 // })
-coding.forEach((val)=>{
-    console.log(val);
-})
+// coding.forEach((val)=>{
+//     console.log(val);
+// })
 
 // function print(val1){
 //     console.log(val1);
